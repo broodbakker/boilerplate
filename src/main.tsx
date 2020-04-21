7aa9c2da9af983b8ad74b3c61227f2448fbbefd8
martijn van de ree <mrtjnvanderee@gmail.com>
2020-04-21 12:45:45 +0200

    6

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,12 @@ export const Main = () => {
       <Button bg="yellow" p={3} color="blue" borderRadius={2}>
         hey >>>>>>> old-state
       </Button>
+      <Button bg="yellow" p={3} color="blue" borderRadius={2}>
+        hey >>>>>>> old-state
+      </Button>
+      <Button bg="yellow" p={3} color="blue" borderRadius={2}>
+        hey >>>>>>> old-state
+      </Button>
     </div>
   );
 };
